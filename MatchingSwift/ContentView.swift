d078f10992120e17acdb0130771eb8c17e75b9bc
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-10-09 02:15:43 +0900

    Initial Commit

---
+++ MatchingSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchingSwift
+//
+//  Created by 菅原大輝 on 2024/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchingSwift/MatchingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingSwiftApp.swift
+//  MatchingSwift
+//
+//  Created by 菅原大輝 on 2024/10/09.
+//
+
+import SwiftUI
+
+@main
+struct MatchingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
